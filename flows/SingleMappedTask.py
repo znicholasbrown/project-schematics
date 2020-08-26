efd754a475e0e4f11727271d2654c9f344e8d868
nicholas <znicholasbrown@gmail.com>
2020-08-26 12:43:36 -0400

    Add single mapped task flow

---
+++ flows/SingleMappedTask.py
@@ -0,0 +1,28 @@
+from prefect import Flow, Task, Parameter
+from prefect.schedules import IntervalSchedule
+from prefect.environments.storage import GitHub
+
+import time
+from datetime import timedelta
+
+
+def MapHandler(Task):
+    def run(self, item):
+        self.logger.info(item)
+        time.sleep()
+        return
+
+
+schedule = IntervalSchedule(interval=timedelta(minutes=30))
+with Flow("Single Mapped Task", schedule=schedule) as flow:
+    parameter = Parameter("no_tasks", default=range(30))
+
+    MapHandler().map(parameter)
+
+
+flow.storage = GitHub(
+    repo="znicholasbrown/project-schematics", path="flows/LocalDaskExecutor.py",
+)
+
+
+flow.register(project_name="PROJECT: Schematics")
