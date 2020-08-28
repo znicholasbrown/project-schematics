08aa881a6d4c8c1597d8bbabcd2ff4e75e989e7e
nicholas <znicholasbrown@gmail.com>
2020-08-28 10:39:45 -0400

    Add class init params

--- flows/PrefectResult.py
+++ flows/PrefectResult.py
@@ -7,7 +7,49 @@ import time
 from datetime import timedelta
 
 
-class Add(Task, result=PrefectResult()):
+class Add(Task):
+    def __init__(
+        self,
+        name=None,
+        slug=None,
+        tags=None,
+        max_retries=None,
+        retry_delay=None,
+        timeout=None,
+        trigger=None,
+        skip_on_upstream_skip=True,
+        cache_for=None,
+        cache_validator=None,
+        cache_key=None,
+        checkpoint=None,
+        result_handler=None,
+        state_handlers=None,
+        on_failure=None,
+        log_stdout=False,
+        result=PrefectResult(),
+        target=None,
+    ):
+        super().__init__(
+            name=name,
+            slug=slug,
+            tags=tags,
+            max_retries=max_retries,
+            retry_delay=retry_delay,
+            timeout=timeout,
+            trigger=trigger,
+            skip_on_upstream_skip=skip_on_upstream_skip,
+            cache_for=cache_for,
+            cache_validator=cache_validator,
+            cache_key=cache_key,
+            checkpoint=checkpoint,
+            result_handler=result_handler,
+            state_handlers=state_handlers,
+            on_failure=on_failure,
+            log_stdout=log_stdout,
+            result=result,
+            target=target,
+        )
+
     def run(self, x, y=1):
         self.logger.info(
             """
