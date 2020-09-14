1e341b19feae8d503f43cef0f1c2fb3ba5ad7f8a
nicholas <znicholasbrown@gmail.com>
2020-09-14 19:01:17 -0400

    Add huge flow

---
+++ flows/hUGe_fLow.py
@@ -0,0 +1,14 @@
+from prefect import Task, Flow
+from prefect.environments.storage import GitHub
+
+with Flow("hUGe fLow") as flow:
+    for i in range(2000):
+        flow.add_task(Task(name=f"{i}"))
+
+
+flow.storage = GitHub(
+    repo="znicholasbrown/project-schematics", path="flows/hUGe_fLow.py",
+)
+
+
+flow.register(project_name="Dev Straining")
