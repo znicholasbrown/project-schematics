ff1418429f2012bab25c3e009deef35e0229217f
nicholas <znicholasbrown@gmail.com>
2020-10-22 11:38:26 -0400

    Remove run_config

--- flows/6 Parallel Tasks.py
+++ flows/6 Parallel Tasks.py
@@ -82,9 +82,4 @@ flow.storage = GitHub(
     secrets=["GITHUB_AUTH_TOKEN"],
 )
 
-flow.run_config = KubernetesRun(
-    labels=["testing", "kubernetes", "run", "config", "labels"]
-)
-
-
 flow.register(project_name="PROJECT: Schematics")
