cf506fc7ada304cbeef1125561edd2f5512094d8
nicholas <znicholasbrown@gmail.com>
2020-11-30 13:49:38 -0500

    Update dependencies

--- flows/0.13.18 Sleeper.py
+++ flows/0.13.18 Sleeper.py
@@ -17,15 +17,7 @@ class Version(Task):
         return
 
 
-class Root(Task):
-    def run(self):
-        self.logger.info("Root running...")
-        time.sleep(random.randint(1, 5))
-        self.logger.info("Root complete.")
-        return list(range(5))
-
-
-class Node(Task):
+class Sleeper(Task):
     def run(self):
         self.logger.info(f"{self.name} running...")
         time.sleep(random.randint(1, 240))
@@ -37,69 +29,69 @@ schedule = IntervalSchedule(interval=timedelta(minutes=5))
 with Flow("0.13.18 Sleeper", schedule=schedule) as flow:
     version = Version()
 
-    root = Root(checkpoint=False)(upstream_tasks=[version])
+    root = Sleeper(checkpoint=False)(upstream_tasks=[version])
 
-    node1_1 = Node(name="Node 1_1", checkpoint=False).map(root)
-    node1_2 = Node(name="Node 1_2", checkpoint=False).map(root)
-    node1_3 = Node(name="Node 1_3", checkpoint=False).map(root)
-    node1_4 = Node(name="Node 1_4", checkpoint=False).map(root)
-    node1_5 = Node(name="Node 1_5", checkpoint=False).map(root)
-    node1_6 = Node(name="Node 1_6", checkpoint=False).map(root)
+    node1_1 = Sleeper(name="Module 1_1", checkpoint=False).map(root)
+    node1_2 = Sleeper(name="Module 1_2", checkpoint=False).map(root)
+    node1_3 = Sleeper(name="Module 1_3", checkpoint=False).map(root)
+    node1_4 = Sleeper(name="Module 1_4", checkpoint=False).map(root)
+    node1_5 = Sleeper(name="Module 1_5", checkpoint=False).map(root)
+    node1_6 = Sleeper(name="Module 1_6", checkpoint=False).map(root)
 
-    node2_1 = Node(name="Node 2_1", checkpoint=False).map(
+    node2_1 = Sleeper(name="Module 2_1", checkpoint=False).map(
         node1_1, upstream_tasks=[node1_1, node1_2]
     )
-    node2_2 = Node(name="Node 2_2", checkpoint=False).map(
+    node2_2 = Sleeper(name="Module 2_2", checkpoint=False).map(
         node1_1, upstream_tasks=[node1_1, node1_2]
     )
-    node2_3 = Node(name="Node 2_3", checkpoint=False).map(
+    node2_3 = Sleeper(name="Module 2_3", checkpoint=False).map(
         node1_1, upstream_tasks=[node1_1, node1_2]
     )
-    node2_4 = Node(name="Node 2_4", checkpoint=False).map(
+    node2_4 = Sleeper(name="Module 2_4", checkpoint=False).map(
         node1_1, upstream_tasks=[node1_1, node1_2]
     )
-    node2_5 = Node(name="Node 2_5", checkpoint=False).map(
+    node2_5 = Sleeper(name="Module 2_5", checkpoint=False).map(
         node1_2, upstream_tasks=[node1_1, node1_2]
     )
-    node2_6 = Node(name="Node 2_6", checkpoint=False).map(
+    node2_6 = Sleeper(name="Module 2_6", checkpoint=False).map(
         node1_2, upstream_tasks=[node1_1, node1_2]
     )
 
-    node3_1 = Node(name="Node 3_1", checkpoint=False).map(
+    node3_1 = Sleeper(name="Module 3_1", checkpoint=False).map(
         node2_1, upstream_tasks=[node2_1]
     )
-    node3_2 = Node(name="Node 3_2", checkpoint=False).map(
+    node3_2 = Sleeper(name="Module 3_2", checkpoint=False).map(
         node2_1, upstream_tasks=[node2_1]
     )
-    node3_3 = Node(name="Node 3_3", checkpoint=False).map(
+    node3_3 = Sleeper(name="Module 3_3", checkpoint=False).map(
         node2_1, upstream_tasks=[node2_1]
     )
-    node3_4 = Node(name="Node 3_4", checkpoint=False).map(
+    node3_4 = Sleeper(name="Module 3_4", checkpoint=False).map(
         node2_1, upstream_tasks=[node2_1]
     )
-    node3_5 = Node(name="Node 3_5", checkpoint=False).map(
+    node3_5 = Sleeper(name="Module 3_5", checkpoint=False).map(
         node2_1, upstream_tasks=[node2_1]
     )
-    node3_6 = Node(name="Node 3_6", checkpoint=False).map(
+    node3_6 = Sleeper(name="Module 3_6", checkpoint=False).map(
         node2_1, upstream_tasks=[node2_1]
     )
 
-    node4_1 = Node(name="Node 4_1", checkpoint=False).map(
+    node4_1 = Sleeper(name="Module 4_1", checkpoint=False).map(
         node3_1, upstream_tasks=[node3_1, node3_2]
     )
-    node4_2 = Node(name="Node 4_2", checkpoint=False).map(
+    node4_2 = Sleeper(name="Module 4_2", checkpoint=False).map(
         node3_1, upstream_tasks=[node3_1, node3_2]
     )
-    node4_3 = Node(name="Node 4_3", checkpoint=False).map(
+    node4_3 = Sleeper(name="Module 4_3", checkpoint=False).map(
         node3_2, upstream_tasks=[node3_1, node3_2]
     )
-    node4_4 = Node(name="Node 4_4", checkpoint=False).map(
+    node4_4 = Sleeper(name="Module 4_4", checkpoint=False).map(
         node3_2, upstream_tasks=[node3_1, node3_2]
     )
-    node4_5 = Node(name="Node 4_5", checkpoint=False).map(
+    node4_5 = Sleeper(name="Module 4_5", checkpoint=False).map(
         node3_2, upstream_tasks=[node3_1, node3_2]
     )
-    node4_6 = Node(name="Node 4_6", checkpoint=False).map(
+    node4_6 = Sleeper(name="Module 4_6", checkpoint=False).map(
         node3_2, upstream_tasks=[node3_1, node3_2]
     )
 
